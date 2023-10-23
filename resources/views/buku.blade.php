af81f15cea818918e93928b0d3c125737368260f
nandithanabiilahp <nandithanabiilahputri1@gmail.com>
2023-10-23 21:27:15 +0700

    Tugas PBKK 2

---
+++ resources/views/buku.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tampilan Buku</title>
+</head>
+<body>
+    <h1>Tampilan Buku</h1>
+    <!-- Buku Laravel -->
+</body>
+</html>
---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tampilan Siswa</title>
+</head>
+<body>
+    <h1>Tampilan Siswa</h1>
+    <!-- Siswa SMA -->
+</body>
+</html>
